a245afdedfddd0222886bd18f1a4e6a2c1bd5274
Gabriel Sens <gabriel_sens@hotmail.com>
2023-01-10 23:27:09 -0300

    Start router and use cases

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import { router } from './router';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
-    app.listen(3001, () => { console.log('Server is running on http://localhost:3001 🔥'); });
+    const port = 3001;
+    app.use(router);
+    app.listen(port, () => { console.log(`Server is running on http://localhost:${port} 🔥`); });
   })
   .catch(() => console.log('erro ao connectar ao mongo'));
